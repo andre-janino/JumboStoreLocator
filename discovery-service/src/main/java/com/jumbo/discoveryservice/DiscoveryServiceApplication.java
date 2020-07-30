bdca1ded1f8449a2b3c7c8f4ec711869a04ffc2e
Janino Andre Armstrong z0032r3a <andre.janino@siemens.com>
2020-07-30 15:31:30 -0300

    Added discovery-service (eureka server) and modified user-service to subscribe to it (eureka client)

---
+++ discovery-service/src/main/java/com/jumbo/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,21 @@
+package com.jumbo.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Spring Boot main class for the Eureka discovery service registry.
+ * 
+ * All hail the all knowing service provider!
+ * 
+ * @author André Janino
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+}
--- user-service/src/main/java/com/jumbo/stores/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/jumbo/stores/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.jumbo.stores.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @EnableJpaRepositories(basePackages = "com.jumbo.stores.userservice.repository")
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
