1bc9e5bd17e1b92fca1241856d62099981ad96f5
Janino Andre Armstrong z0032r3a <andre.janino@siemens.com>
2020-07-31 21:14:58 -0300

    Small changes on the api-gateway to enable oauth in the future.

--- api-gateway/src/main/java/com/jumbo/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jumbo/apigateway/ApiGatewayApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * API Gateway spring application.
+ * 
+ * @author André Janino
+ */
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
