e4ce52d0873cb302fc85ac268117e57d470d1a10
Janino Andre Armstrong z0032r3a <andre.janino@siemens.com>
2020-08-07 15:50:17 -0300

    Added new test users.

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -14,5 +14,14 @@ insert into role (id, name) values(3, 'ADMIN');
 insert into users (id, email, first_name, last_name, password, role_id)
 values(1001, 'andre.janino@gmail.com', 'André', 'Armstrong Janino Cizotto', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
 
-insert into users (id, email, first_name, password, role_id)
-values(1002, 'jumbotest@jumbo.com', 'Jumbo', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 2);
+insert into users (id, email, first_name, last_name, password, role_id)
+values(1002, 'marijn.deromph@jumbo.com', 'Marijn', 'de Römph', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
+
+insert into users (id, email, first_name, last_name, password, role_id)
+values(1003, 'marijn.deromph@jumbo.com', 'Marijn', 'de Römph', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
+
+insert into users (id, email, first_name, last_name, password, role_id)
+values(1004, 'tamara.duric@jumbo.com', 'Tamara', 'Duric', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
+
+insert into users (id, email, first_name, last_name, password, role_id)
+values(1005, 'gustavo.henriquesmartins@jumbo.com', 'Gustavo', 'Henrique Martins', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
