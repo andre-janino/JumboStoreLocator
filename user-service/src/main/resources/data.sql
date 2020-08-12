2b46357286d0e163ae89374a3bc563f9607df88d
Janino Andre Armstrong z0032r3a <andre.janino@siemens.com>
2020-08-12 19:01:18 -0300

    Modified the build name of the jars.

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -12,7 +12,7 @@ insert into role (id, name) values(2, 'USER');
 insert into role (id, name) values(3, 'ADMIN');
 
 insert into users (id, email, first_name, last_name, password, role_id)
-values(1001, 'andre.janino@gmail.com', 'André', 'Armstrong Janino Cizotto', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
+values(1001, 'andre.janino@gmail.com', 'Andre', 'Armstrong Janino Cizotto', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
 
 insert into users (id, email, first_name, last_name, password, role_id)
 values(1002, 'marijn.deromph@jumbo.com', 'Marijn', 'de Römph', '$2a$10$heirHA89ULwxENiWxaj25O1S9oRafpyvLQw21shSNWhV7i/VuZNJ6', 3);
